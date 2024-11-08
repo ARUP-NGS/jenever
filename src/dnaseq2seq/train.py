6c7d3f6080dbfc72f8999f1fda9841dd285d951c
Brendan <22319@MLAGNURIAI03D.aruplab.net>
2024-11-07 19:35:23 -0700

    compile

--- src/dnaseq2seq/train.py
+++ src/dnaseq2seq/train.py
@@ -350,8 +350,8 @@ def load_model(modelconf, ckpt):
     #model.fc1.requires_grad_(False)
     #model.fc2.requires_grad_(False)
     
-    # logger.info("Compiling model...")
-    # model = torch.compile(model)
+    logger.info("Compiling model...")
+    model = torch.compile(model)
     
     if USE_DDP:
         rank = dist.get_rank()
