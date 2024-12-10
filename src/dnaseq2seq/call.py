0bde0df439a5d96f0737aa1b893b0fff6e86775a
Brendan <22319@MLAGNURIAI03D.aruplab.net>
2024-12-10 10:28:28 -0700

    Get max_read_depth from modelconf for calling

--- src/dnaseq2seq/call.py
+++ src/dnaseq2seq/call.py
@@ -140,7 +140,7 @@ def load_model(model_path):
     model.to(DEVICE)
     
     model = torch.compile(model, fullgraph=True)
-    return model
+    return model, modelconf
 
 
 def cluster_positions_for_window(window, bamfile, reference_fasta, maxdist=100):
@@ -211,9 +211,6 @@ def call(model_path: str, bam: str, bed: str, reference_fasta: str, vcf_out: str
         logger.info("No classifier model provided, emitting uncalibrated qualities only. Specificity will be poor")
     else:
         assert Path(classifier_path).is_file(), f"Classifier model {classifier_path} isn't a regular file"
-
-    # Verify model loading, we just want to fail fast here if there's an issue
-    load_model(model_path)
 
     call_vars_in_parallel(
         bampath=bam,
@@ -289,6 +286,10 @@ def call_vars_in_parallel(
     bed_chrom_order = util.unique_chroms(bed)
     priority_func = partial(region_priority, chrom_order=bed_chrom_order)
     progress_tracker = util.RegionProgressCounter(bed)
+    
+    # Verify model loading, we just want to fail fast here if there's an issue
+    _, modelconf = load_model(model_path)
+
 
     with mp.Manager() as manager:
         callstate = manager.dict()
@@ -301,7 +302,7 @@ def call_vars_in_parallel(
         region_finder.start()
 
         region_workers = [mp.Process(target=worker_wrapper,
-                                     args=(generate_tensors, callstate, regions_queue, tensors_queue, bampath, refpath, callstate))
+                                     args=(generate_tensors, callstate, regions_queue, tensors_queue, bampath, refpath, callstate, modelconf['max_read_depth']))
                           for _ in range(threads)]
 
         for p in region_workers:
@@ -520,7 +521,7 @@ def accumulate_regions_and_call(modelpath: str,
     """
 
     torch.set_num_threads(8)
-    model = load_model(modelpath)
+    model, _ = load_model(modelpath)
     model.eval()
     if classifier_path:
         classifier = buildclf.load_model(classifier_path)
