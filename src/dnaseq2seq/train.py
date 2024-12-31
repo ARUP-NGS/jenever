fdbc6db348fb96ba10da9e2008f2e89bd3d30e79
O'Fallon, Brendan <brendan.o'fallon@aruplab.com>
2024-12-31 15:03:20 -0700

    Fix val loader thing again

--- src/dnaseq2seq/train.py
+++ src/dnaseq2seq/train.py
@@ -425,10 +425,10 @@ def train_epochs(model,
                  optimizer,
                  epochs,
                  dataloader,
+                 val_loader,
                  scheduler,
                  checkpoint_freq=0,
                  model_dest=None,
-                 val_dir=None,
                  batch_size=64,
                  xtra_checkpoint_items={},
                  samples_per_epoch=10000,
@@ -446,16 +446,6 @@ def train_epochs(model,
             "mean_var_count", "ppa_dels", "ppa_ins", "ppa_snv",
             "ppv_dels", "ppv_ins", "ppv_snv", "learning_rate", "epochtime",
     ])
-
-
-    if val_dir:
-        logger.info(f"Using validation data in {val_dir}")
-        val_loader = loader.PregenLoader(device=DEVICE, datadir=val_dir, max_decomped_batches=4, threads=8, tgt_prefix="tgkmers")
-    else:
-        logger.info(f"No val. dir. provided retaining a few training samples for validation")
-        valpaths = dataloader.retain_val_samples(fraction=0.05)
-        val_loader = loader.PregenLoader(device=DEVICE, datadir=None, pathpairs=valpaths, threads=4, tgt_prefix="tgkmers")
-        logger.info(f"Pulled {len(valpaths)} samples to use for validation")
 
     try:
         sample_iter = iter_indefinitely(dataloader, batch_size)
@@ -668,7 +658,10 @@ def train(output_model, **kwargs):
 
     logger.info(f"Truncating max read depth to {model_unwrapped.read_depth}")
     dataloader = loader.TruncateDepthLoader(dataloader, model_unwrapped.read_depth)
-
+    val_loader = loader.TruncateDepthLoader(
+        loader.PregenLoader(DEVICE, kwargs.get('val_dir'), max_decomped_batches=8, threads=8, tgt_prefix="tgkmers"),
+        model_unwrapped.read_depth
+    )
 
     if kwargs.get('model_encoder_fix'):
         logger.info(f"Loading and freezing encoder from {kwargs['model_encoder_fix']}")
@@ -707,10 +700,10 @@ def train(output_model, **kwargs):
                  optimizer,
                  kwargs.get('epochs'),
                  dataloader,
+                 val_loader,
                  scheduler=scheduler,
                  model_dest=output_model,
                  checkpoint_freq=kwargs.get('checkpoint_freq', 10),
-                 val_dir=kwargs.get('val_dir'),
                  batch_size=kwargs.get("batch_size"),
                  samples_per_epoch=kwargs.get('samples_per_epoch'),
                  xtra_checkpoint_items=kwargs['model'],
