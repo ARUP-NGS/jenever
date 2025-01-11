28933132cff26c889b475fe467d8c4608ae9ba00
O'Fallon, Brendan <brendan.o'fallon@aruplab.com>
2025-01-11 12:59:52 -0700

    Try buffering around ref seq aln

--- src/dnaseq2seq/hapmerger.py
+++ src/dnaseq2seq/hapmerger.py
@@ -194,8 +194,8 @@ def align_and_merge_haplotypes(haplotypes: List[Tuple[str, np.array, int]], ref_
 
     refmaps = []
     for i, (hapseq, probs, ref_offset) in enumerate(haplotypes):
-        seq_offset = ref_offset - ref_start
-        ssw = StripedSmithWaterman(ref_seq[seq_offset:seq_offset+len(hapseq)],
+        seq_offset = max(0, ref_offset - ref_start - 10)
+        ssw = StripedSmithWaterman(ref_seq[seq_offset:seq_offset+len(hapseq) + 10],
                             gap_open_penalty=5,
                             gap_extend_penalty=0.1,
                             match_score=1,
